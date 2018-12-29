3023388eea472792cd3eaa841c5e47fcc0e58060
lihongshuai <lihongshuai@longcheer.com>
2018-12-29 16:17:45 +0800

    [B810][TP][NA] 添加 TP driver
    [rootcause] No
    [solution] 添加TP driver
    [sideeffects] No
    [others] No
    
    Change-Id: Id468c2b02a13fe894731a2bcd03f2d57d94c0711

--- drivers/input/touchscreen/focaltech_touch/focaltech_i2c.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_i2c.c
@@ -2,7 +2,7 @@
  *
  * FocalTech TouchScreen driver.
  *
- * Copyright (c) 2010-2017, FocalTech Systems, Ltd., all rights reserved.
+ * Copyright (c) 2012-2018, FocalTech Systems, Ltd., all rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -16,191 +16,225 @@
  */
 
 /************************************************************************
- *
- * File Name: focaltech_i2c.c
- *
- *    Author: fupeipei
- *
- *   Created: 2016-08-04
- *
- *  Abstract: i2c communication with TP
- *
- *   Version: v1.0
- *
- * Revision History:
- *        v1.0:
- *            First release. By fupeipei 2016-08-04
- ************************************************************************/
+*
+* File Name: focaltech_i2c.c
+*
+* Author: Focaltech Driver Team
+*
+* Created: 2016-08-04
+*
+* Abstract: i2c communication with TP
+*
+* Version: v1.0
+*
+* Revision History:
+*
+************************************************************************/
 
 /*****************************************************************************
- * Included header files
- *****************************************************************************/
+* Included header files
+*****************************************************************************/
 #include "focaltech_core.h"
 
 /*****************************************************************************
- * Private constant and macro definitions using #define
- *****************************************************************************/
+* Private constant and macro definitions using #define
+*****************************************************************************/
+#define I2C_RETRY_NUMBER        3
+/*****************************************************************************
+* Private enumerations, structures and unions using typedef
+*****************************************************************************/
 
 /*****************************************************************************
- * Private enumerations, structures and unions using typedef
- *****************************************************************************/
-
-/*****************************************************************************
- * Static variables
- *****************************************************************************/
+* Static variables
+*****************************************************************************/
 static DEFINE_MUTEX(i2c_rw_access);
 
 /*****************************************************************************
- * Global variable or extern global variabls/functions
- *****************************************************************************/
+* Global variable or extern global variabls/functions
+*****************************************************************************/
 
 /*****************************************************************************
- * Static function prototypes
- *****************************************************************************/
+* Static function prototypes
+*****************************************************************************/
 
 /*****************************************************************************
- * functions body
- *****************************************************************************/
+* functions body
+*****************************************************************************/
 
 /************************************************************************
- * Name: fts_i2c_read
- * Brief: i2c read
- * Input: i2c info, write buf, write len, read buf, read len
- * Output: get data in the 3rd buf
- * Return: fail <0
- ***********************************************************************/
-int fts_i2c_read(struct i2c_client *client, char *writebuf,
-		int writelen, char *readbuf, int readlen)
+* Name: fts_i2c_read
+* Brief: i2c read
+* Input: i2c info, write buf, write len, read buf, read len
+* Output: get data in the 3rd buf
+* Return: fail <0
+***********************************************************************/
+int fts_i2c_read(struct i2c_client *client, char *writebuf, int writelen, char *readbuf, int readlen)
 {
-	int ret = -EIO;
+    int ret = 0;
+    int i = 0;
 
-	mutex_lock(&i2c_rw_access);
+    mutex_lock(&i2c_rw_access);
 
-	if (readlen > 0) {
-		if (writelen > 0) {
-			struct i2c_msg msgs[] = {
-				{
-					.addr = client->addr,
-					.flags = 0,
-					.len = writelen,
-					.buf = writebuf,
-				},
-				{
-					.addr = client->addr,
-					.flags = I2C_M_RD,
-					.len = readlen,
-					.buf = readbuf,
-				},
-			};
-			ret = i2c_transfer(client->adapter, msgs, 2);
-			if (ret < 0)
-				FTS_ERROR("[IIC]: i2c_write error %d!!", ret);
-		} else {
-			struct i2c_msg msgs[] = {
-				{
-					.addr = client->addr,
-					.flags = I2C_M_RD,
-					.len = readlen,
-					.buf = readbuf,
-				},
-			};
-			ret = i2c_transfer(client->adapter, msgs, 1);
-			if (ret < 0)
-				FTS_ERROR("[IIC]: i2c_read error %d!!", ret);
-		}
-	}
+    if (readlen > 0) {
+        if (writelen > 0) {
+            struct i2c_msg msgs[] = {
+                {
+                    .addr = client->addr,
+                    .flags = 0,
+                    .len = writelen,
+                    .buf = writebuf,
+                },
+                {
+                    .addr = client->addr,
+                    .flags = I2C_M_RD,
+                    .len = readlen,
+                    .buf = readbuf,
+                },
+            };
+            for (i = 0; i < I2C_RETRY_NUMBER; i++) {
+                ret = i2c_transfer(client->adapter, msgs, 2);
+                if (ret < 0) {
+                    FTS_ERROR("[IIC]: i2c_transfer(write) error, ret=%d!!", ret);
+                } else
+                    break;
+            }
+        } else {
+            struct i2c_msg msgs[] = {
+                {
+                    .addr = client->addr,
+                    .flags = I2C_M_RD,
+                    .len = readlen,
+                    .buf = readbuf,
+                },
+            };
+            for (i = 0; i < I2C_RETRY_NUMBER; i++) {
+                ret = i2c_transfer(client->adapter, msgs, 1);
+                if (ret < 0) {
+                    FTS_ERROR("[IIC]: i2c_transfer(read) error, ret=%d!!", ret);
+                } else
+                    break;
+            }
+        }
+    }
 
-	mutex_unlock(&i2c_rw_access);
-
-	return ret;
+    mutex_unlock(&i2c_rw_access);
+    return ret;
 }
 
 /************************************************************************
- * Name: fts_i2c_write
- * Brief: i2c write
- * Input: i2c info, write buf, write len
- * Output: no
- * Return: fail <0
- ***********************************************************************/
+* Name: fts_i2c_write
+* Brief: i2c write
+* Input: i2c info, write buf, write len
+* Output: no
+* Return: fail <0
+***********************************************************************/
 int fts_i2c_write(struct i2c_client *client, char *writebuf, int writelen)
 {
-	int ret = 0;
+    int ret = 0;
+    int i = 0;
 
-	mutex_lock(&i2c_rw_access);
+    mutex_lock(&i2c_rw_access);
+    if (writelen > 0) {
+        struct i2c_msg msgs[] = {
+            {
+                .addr = client->addr,
+                .flags = 0,
+                .len = writelen,
+                .buf = writebuf,
+            },
+        };
+        for (i = 0; i < I2C_RETRY_NUMBER; i++) {
+            ret = i2c_transfer(client->adapter, msgs, 1);
+            if (ret < 0) {
+                FTS_ERROR("%s: i2c_transfer(write) error, ret=%d", __func__, ret);
+            } else
+                break;
+        }
+    }
+    mutex_unlock(&i2c_rw_access);
 
-	if (writelen > 0) {
-		struct i2c_msg msgs[] = {
-			{
-				.addr = client->addr,
-				.flags = 0,
-				.len = writelen,
-				.buf = writebuf,
-			},
-		};
-		ret = i2c_transfer(client->adapter, msgs, 1);
-		if (ret < 0)
-			FTS_ERROR("[IIC]: i2c_write error, ret=%d", ret);
-	}
-
-	mutex_unlock(&i2c_rw_access);
-
-	return ret;
+    return ret;
 }
 
 /************************************************************************
- * Name: fts_i2c_write_reg
- * Brief: write register
- * Input: i2c info, reg address, reg value
- * Output: no
- * Return: fail <0
- ***********************************************************************/
+* Name: fts_i2c_write_reg
+* Brief: write register
+* Input: i2c info, reg address, reg value
+* Output: no
+* Return: fail <0
+***********************************************************************/
 int fts_i2c_write_reg(struct i2c_client *client, u8 regaddr, u8 regvalue)
 {
-	u8 buf[2] = {0};
+    u8 buf[2] = {0};
 
-	buf[0] = regaddr;
-	buf[1] = regvalue;
-	return fts_i2c_write(client, buf, sizeof(buf));
+    buf[0] = regaddr;
+    buf[1] = regvalue;
+    return fts_i2c_write(client, buf, sizeof(buf));
 }
 
 /************************************************************************
- * Name: fts_i2c_read_reg
- * Brief: read register
- * Input: i2c info, reg address, reg value
- * Output: get reg value
- * Return: fail <0
- ***********************************************************************/
+* Name: fts_i2c_read_reg
+* Brief: read register
+* Input: i2c info, reg address, reg value
+* Output: get reg value
+* Return: fail <0
+***********************************************************************/
 int fts_i2c_read_reg(struct i2c_client *client, u8 regaddr, u8 *regvalue)
 {
-	return fts_i2c_read(client, &regaddr, 1, regvalue, 1);
+    return fts_i2c_read(client, &regaddr, 1, regvalue, 1);
 }
 
 /************************************************************************
- * Name: fts_i2c_init
- * Brief: fts i2c init
- * Input:
- * Output:
- * Return:
- ***********************************************************************/
+* HID to standard I2C
+***********************************************************************/
+void fts_i2c_hid2std(struct i2c_client *client)
+{
+    int ret = 0;
+    u8 buf[3] = {0xeb, 0xaa, 0x09};
+
+    ret = fts_i2c_write(client, buf, 3);
+    if (ret < 0)
+        FTS_ERROR("hid2std cmd write fail");
+    else {
+        msleep(10);
+        buf[0] = buf[1] = buf[2] = 0;
+        ret = fts_i2c_read(client, NULL, 0, buf, 3);
+        if (ret < 0)
+            FTS_ERROR("hid2std cmd read fail");
+        else if ((0xeb == buf[0]) && (0xaa == buf[1]) && (0x08 == buf[2])) {
+            FTS_DEBUG("hidi2c change to stdi2c successful");
+        } else {
+            FTS_ERROR("hidi2c change to stdi2c fail");
+        }
+    }
+}
+
+/************************************************************************
+* Name: fts_i2c_init
+* Brief: fts i2c init
+* Input:
+* Output:
+* Return:
+***********************************************************************/
 int fts_i2c_init(void)
 {
-	FTS_FUNC_ENTER();
+    FTS_FUNC_ENTER();
 
-	FTS_FUNC_EXIT();
-	return 0;
+    FTS_FUNC_EXIT();
+    return 0;
 }
 /************************************************************************
- * Name: fts_i2c_exit
- * Brief: fts i2c exit
- * Input:
- * Output:
- * Return:
- ***********************************************************************/
+* Name: fts_i2c_exit
+* Brief: fts i2c exit
+* Input:
+* Output:
+* Return:
+***********************************************************************/
 int fts_i2c_exit(void)
 {
-	FTS_FUNC_ENTER();
+    FTS_FUNC_ENTER();
 
-	FTS_FUNC_EXIT();
-	return 0;
+    FTS_FUNC_EXIT();
+    return 0;
 }
 
